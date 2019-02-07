4ced25b1a7fa7a9e80118f54214273cf3ce56d17
computer0101 <deveshpatel7643@gmail.com>
2019-02-08 03:44:42 +0530

    few improvements in header styles

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -3,12 +3,25 @@ import { Link } from 'react-router-dom';
 import './Header.css';
 
 class Header extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      active: 'Home'
+    }
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleClick(e) {
+    const active = e.target.text;
+    this.setState(() => ({ active }));
+  }
+
   render() {
     return (
       <div className='header'>
-        <Link className='header-link active' to='/'>Home</Link>
-        <Link className='header-link' to='/about'>About us</Link>
-        <Link className='header-link' to='/contact'>Contact us</Link>
+        <Link className={`header-link ${this.state.active === 'Home' ? 'active' : ''}`} to='/' onClick={this.handleClick}>Home</Link>
+        <Link className={`header-link ${this.state.active === 'About us' ? 'active' : ''}`} to='/about' onClick={this.handleClick}>About us</Link>
+        <Link className={`header-link ${this.state.active === 'Contact us' ? 'active' : ''}`} to='/contact' onClick={this.handleClick}>Contact us</Link>
       </div>
     );
   }
